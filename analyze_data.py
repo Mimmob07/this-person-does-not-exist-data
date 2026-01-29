b340e1526fcae2e996429b76594257bb97464577
Mimmob07 <mimmo@umich.edu>
2026-01-29 00:02:55 -0500

    Analyzed data

---
+++ analyze_data.py
@@ -0,0 +1,21 @@
+# from hair_color_detector import HairColorDetector
+
+# hcd = HairColorDetector()
+# detected_color = hcd.get_color("assets/img_1.jpeg", save_result=False)
+# print(f"Detected color: {detected_color}")
+
+from deepface import DeepFace
+
+file = open("data.csv", "a")
+file.write("Filename,Race,Emotion,Gender,Age\n")
+
+for i in range(1,1001):
+    filename = f"assets/img_{i}.jpeg"
+
+    try:
+        objs = DeepFace.analyze(img_path = filename, actions = ['age', 'gender', 'race', 'emotion'])[0]
+        file.write(f"{filename},{objs['dominant_race']},{objs['dominant_emotion']},{objs['dominant_gender']},{objs['age']}\n")
+        print(f"Processed image {i}")
+    except Exception as e:
+        print(f"Face not found: {e}")
+        file.write(f"{filename},,,,\n")
--- gather_data.py
+++ gather_data.py
@@ -4,7 +4,7 @@ url = "https://thispersondoesnotexist.com/"
 # Spoof chrome on a windows 10 computer
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
 
-for i in range(1, 1000):
+for i in range(1, 1001):
     filename = f"assets/img_{i}.jpeg"
     request = urllib.request.Request(url, headers=headers)
 
