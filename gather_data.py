eb42b87621a13796b06cf5acc4852bbe2a7e5748
Mimmob07 <mimmo@umich.edu>
2026-01-28 21:36:53 -0500

    Gathered data

---
+++ gather_data.py
@@ -0,0 +1,14 @@
+import urllib.request
+
+url = "https://thispersondoesnotexist.com/"
+# Spoof chrome on a windows 10 computer
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'}
+
+for i in range(1, 1000):
+    filename = f"assets/img_{i}.jpeg"
+    request = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(request) as response, open(filename, "wb") as file:
+        file.write(response.read())
+
+    print(f"Saved file {filename}")
